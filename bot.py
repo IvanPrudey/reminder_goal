83efd2fd55aa808a208b4e75c87e214e38c47534
IvanPrudey <prudey.ia@yandex.ru>
2025-12-11 19:06:42 +0300

    add_start_handler

--- bot.py
+++ bot.py
@@ -13,5 +13,13 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot=bot)
 
+
+@dp.message_handler(commands=['start'])
+async def start_handler(message: types.Message):
+    user_id = message.from_user.id
+    user_full_name = message.from_user.full_name
+    logging.INFO(f'{user_id=} {user_full_name=} {time.asctime()}')
+    await message.reply(f'Привет, {user_full_name}')
+
 if __name__ == '__main__':
     pass
