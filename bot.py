1a235839c5feca3faa07df848f40d54862ff6ba9
IvanPrudey <prudey.ia@yandex.ru>
2025-12-12 18:19:31 +0300

    correct_logging

--- bot.py
+++ bot.py
@@ -16,6 +16,10 @@ logging.basicConfig(
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+if not BOT_TOKEN:
+    logging.error('BOT_TOKEN не найден в .env файле!')
+    exit(1)
+
 MSG = '{}, ты сегодня кодил?'
 
 bot = Bot(token=BOT_TOKEN)
@@ -27,12 +31,16 @@ async def start_handler(message: Message):
     user_id = message.from_user.id
     user_name = message.from_user.first_name
     user_full_name = message.from_user.full_name
-    logging.info(f'{user_id=} {user_full_name=} {time.asctime()}')
-    await message.reply(f'Привет, {user_full_name}')
+    logging.info(f'Пользователь {user_id=} {user_full_name=} запустил бота {time.asctime()}')
+    await message.reply(f'Привет, {user_full_name}, я буду напоминать тебе о кодинге!')
 
-    for i in range(10):
-        await asyncio.sleep(2)
-        await bot.send_message(user_id, MSG.format(user_name))
+    for i in range(7):
+        await asyncio.sleep(60*60*24)
+        try:
+            await bot.send_message(user_id, MSG.format(user_name))
+            logging.info(f'Отправлено напоминание в {i+1}/7 дней')
+        except Exception as e:
+            logging.error(f'Ошибка при отправке сообщения: {e}')
 
 
 async def main():
